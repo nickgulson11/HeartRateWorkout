9638d08932fba005e7efcd9903068105d72801fd
Nick Gulson <nicholasgulson2021@u.northwestern.edu>
2020-12-07 15:54:25 -0600

    Final Changes

--- creategraph.py
+++ creategraph.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 
 def graph(data):
     plt.switch_backend('Agg')
-    plt.plot([50,100,150,200,250], data)
+    data_int = [int(data[0]),int(data[1]),int(data[2]),int(data[3]),int(data[4])]
+    plt.plot([50,100,150,200,250], data_int)
     plt.xlabel('Number of Jumping Jacks')
     plt.ylabel('Heat Rate (BPM)')
     plt.title("Heart Rate as Workouts Get Harder")
